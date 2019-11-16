10262ad174436cceb394e9dc63a8abd25c0b4e84
William Riddle <wtriddle31@gmail.com>
2019-11-15 18:53:11 -0800

    subtle changes for complete functionality, ontop of previous commit

--- smiles.py
+++ smiles.py
@@ -4,12 +4,6 @@ import re
 for line in open('SMILES.txt', 'r'):
     lineInfo = re.compile(r'\S+').findall(line)
     smiles = lineInfo[0]
-    # posCounter = -1
-    # print(" The length of the smiles code is ", len(smiles))
-    # for symbol in smiles:
-    #     posCounter += 1
-    #     print(symbol, " is in position ", posCounter)
-    #     print("Chopped smiles string is ", smiles[0:posCounter+1])
     functionalGroupData = ifg.ifg(smiles)
     print(functionalGroupData)
     break
