3370e09101b8261cd9e5bebbca87fcf7dde69090
William Riddle <wtriddle31@gmail.com>
2019-11-16 08:36:04 -0800

    Added a functional group to the list, allow smiles.py to loop through every code in SMILES.txt

--- smiles.py
+++ smiles.py
@@ -4,6 +4,6 @@ import re
 for line in open('SMILES.txt', 'r'):
     lineInfo = re.compile(r'\S+').findall(line)
     smiles = lineInfo[0]
+    print("EVALUATING ", lineInfo[1])
     functionalGroupData = ifg.ifg(smiles)
     print(functionalGroupData)
-    break
