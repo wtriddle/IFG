1ee76bcda233a3afe946ffb90b9181e232cea8b3
William Riddle <wtriddle31@gmail.com>
2019-12-20 07:47:29 -0800

    Slight changes and additions, nothing to impact IFG algorithm

--- smiles.py
+++ smiles.py
@@ -1,11 +1,12 @@
 import ifg
 import re
 
-for line in open('SMILES.txt', 'r'):
-    lineInfo = re.compile(r'\S+').findall(line)
-    smiles = lineInfo[0]
-    print("EVALUATING ", lineInfo[1])
-    functionalGroupData = ifg.ifg(smiles)
-    print(functionalGroupData)
-# functionalGroupData = ifg.ifg("CN1C(=O)N(C)c2nc3cccc(C)c3nc2C1=O")
-# print(functionalGroupData)
+# for line in open('SMILES.txt', 'r'):
+#     lineInfo = re.compile(r'\S+').findall(line)
+#     smiles = lineInfo[0]
+#     print("EVALUATING ", lineInfo[1])
+#     functionalGroupData = ifg.ifg(smiles)
+#     print(functionalGroupData)
+functionalGroupData = ifg.ifg("[N-]=[N+]=NC1=NN=NN1N=C1CCC(CC1)=NN1N=NN=C1N=[N+]=[N-]")
+print(functionalGroupData)
+print("[N-]=[N+]=NC1=NN=NN1N=C1CCC(CC1)=NN1N=NN=C1N=[N+]=[N-]")
