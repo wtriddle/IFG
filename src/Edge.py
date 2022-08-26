61c86b3e1118557437cd3aff112aac148987d59e
William Riddle <wtriddle31@gmail.com>
2022-08-26 13:09:10 -0700

    edge class with index and vertex objects

--- src/Edge.py
+++ src/Edge.py
@@ -4,6 +4,7 @@ Edge bond_type is the type of bond between two verticies (i.e atoms) in the mole
 Edge verticies are the two atom symbols which are connected by the edge
 Edge indices are the two atom index values which identify the two atoms that are connected by the edge (local to the graph)
 Edge core_type is the assertion of both verticies being non-R atoms (the negation of core_type is called an R_type which has an R atom involved in the edge connection)
+Edge index is the index of the edge in the graph
 """
 
 from typing import Literal, TypeVar
@@ -14,13 +15,16 @@ EdgeType = TypeVar('EdgeType', bound='Edge')
 class Edge(object):
 
     def __init__(self, 
-        atoms: "list[Vertex]",
+        vertices: "list[Vertex]",
         bond_type: Literal["", "=", "#"], 
+        index: int
     ) -> None:
+        self.index: int = index
+        self.atoms: "list[Vertex]" = vertices
         self.bond_type: Literal["", "=", "#"] = bond_type
-        self.vertices: list[str] = list(str(atom.symbol) for atom in atoms)
-        self.indices: list[int] = list(int(atom.index) for atom in atoms)
-        self.core_type: bool = not 'R' in self.vertices
+        self.symbols: list[str] = list(str(vertex.symbol) for vertex in vertices)
+        self.indices: list[int] = list(int(vertex.index) for vertex in vertices)
+        self.core_type: bool = not 'R' in self.symbols
 
     def __eq__(self: EdgeType, __o: EdgeType) -> bool:
         # Validates structural edge equality
@@ -29,17 +33,27 @@ class Edge(object):
 
         # core_type edge matching
         if (self.core_type):
-            return (self.bond_type == __o.bond_type) & (self.vertices == __o.vertices)
+            return (self.bond_type == __o.bond_type) and (set(self.symbols) == set(__o.symbols))
 
         # R_type edge matching
-        return (self.bond_type == __o.bond_type) & (self.vertices.remove('R') in __o.vertices)
+        return (self.bond_type == __o.bond_type) and ([v for v  in self.symbols if v != 'R'][0] in __o.symbols)
+    
+    def __hash__(self):
+        return hash((self.bond_type, self.symbols[0], self.symbols[1], self.core_type))
 
     def __str__(self):
-        return self.bond_type.join([''.join([str(r) for r in list(z)]) for z in zip(self.vertices, self.indices)])
+        return self.bond_type.join([''.join([str(r) for r in list(z)]) for z in zip(self.symbols, self.indices)])
 
     def __repr__(self):
-        return self.bond_type.join([''.join([str(r) for r in list(z)]) for z in zip(self.vertices, self.indices)])
+        return self.bond_type.join([''.join([str(r) for r in list(z)]) for z in zip(self.symbols, self.indices)])
 
     def mirror(self: EdgeType, __o: EdgeType):
         # Validates a mirrored edge equality (i.e if two edge objects are the exact same edge in a particular graph)
-        return (self.bond_type == __o.bond_type) & (self.vertices == __o.vertices) & (self.indices == __o.indices)
+        return (self.bond_type == __o.bond_type) & (self.symbols == __o.symbols) & (self.indices == __o.indices)
+
+    def complement_vertex(self, vertex_index):
+        # gets the complement vertex in the edge when given a specific vertex_index
+        return [vertex for vertex in self.atoms if vertex.index != vertex_index][0]
+        # try:
+        # except:
+        #     ValueError("There was an error in the complement calculations")
