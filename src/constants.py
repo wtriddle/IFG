a7de0e343e83b9eaed61b14312e8ff10bbe55094
William Riddle <wtriddle31@gmail.com>
2022-08-26 13:09:43 -0700

    updated regex and valence counting constants

--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,31 @@
 import re
 
 ATOM_REGEX = re.compile(r'Br[+-]?|Cl[+-]?|[a-zA-Z][+-]?')
-CHARGE_REGEX = re.compile(r'\+|\-')
+CHARGE_REGEX = re.compile(r'[+-]{1}')
 BOND_REGEX = re.compile(r'[=#]')
 DIGIT_REGEX = re.compile(r'[0-9]{1}')
 PARENTH_REGEX = re.compile(r'[()]')
 BRACKET_REGEX = re.compile(r'[\[\]]')
-SMILES_REGEX = re.compile(r'Br[+-]?|Cl[+-]?|H[0-9]?[+-]?|[a-zA-Z][+-]?|[=#]|[0-9]{1}|[()]')
+SMILES_REGEX = re.compile(r'(Br[+-]?|Cl[+-]?|[a-zA-Z][+-]?|[=#]|[0-9]{1}|[()])')
 AMINO_ACID_REGEX = re.compile(r'[nN]H[23]?\+')
+
+
+VALENCE_COUNTS = {
+    'C': 4,
+    'N': 3,
+    'P': 3,
+    'O': 2,
+    'S': 2,
+    'SE': 2,
+    'F': 1,
+    'CL': 1,
+    'BR': 1,
+    'I': 1,
+    'R': 1
+}
+
+ELECTRON_BOND_COUNTS = {
+    '': 1,
+    '=': 2,
+    '#': 3
+}
