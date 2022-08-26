abb8c13b81c0998435bba8242c8db808c71ed4d2
William Riddle <wtriddle31@gmail.com>
2022-08-26 13:11:21 -0700

    vertex degrees and valence electrons

--- src/Vertex.py
+++ src/Vertex.py
@@ -10,10 +10,19 @@ from typing import Literal
 
 class Vertex():
 
-    def __init__(self, index: int, symbol: str, isAromatic: bool = False):
+    def __init__(self, index: int = 0, symbol: str = 'C', isAromatic: bool = False, valenceElectronsRequired: int = 0, charge: str = ""):
         self.index: int = index
         self.symbol: str = symbol
         self.ring_type: Literal["aromatic", "non-aromatic", "non-cyclic"] = "aromatic" if isAromatic else "non-cyclic"
+        self.implicit_degree: int = 0               # the number of single bonded hydrogens
+        self.explicit_degree: int = 0               # the number of explicitly determined edges from the SMILES code
+        self.total_degree: int = self.explicit_degree + self.implicit_degree # the sum of the explicit and implicit degrees
+        self.valence_electrons_required: int = valenceElectronsRequired
+        self.charge = charge
+
+        ##### Charge Valence Electrons #####
+        if charge == '+': self.valence_electrons_required+=1
+        elif charge == '-': self.valence_electrons_required-=1
 
     def __eq__(self, value):
         """ Same symbol and index imply equivalent atoms """
